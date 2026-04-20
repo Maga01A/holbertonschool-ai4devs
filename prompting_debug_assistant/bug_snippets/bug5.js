4e70238f038ef22ccc307e833f7e07efe5941c6c
Maga01A <alnatluxanov@gmail.com>
2026-04-20 13:48:23 +0400

    ai4devs

--- prompting_debug_assistant/bug_snippets/bug5.js
+++ prompting_debug_assistant/bug_snippets/bug5.js
@@ -2,7 +2,14 @@ function addTen(value) {
     return value + 10;
 }
 
-let input = "5";
-let result = addTen(input);
+function testAddTen() {
+    let input = "5";
+    let result = addTen(input);
+    console.log("Result:", result);
+}
 
-console.log("Result:", result);
+function anotherTest() {
+    console.log(addTen("10"));
+}
+
+testAddTen();
